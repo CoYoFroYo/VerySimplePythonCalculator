5c287d653518637cb3c7f3b55f9fcd8b15968ddd
Cody York <clyork@bsu.edu>
2018-01-12 10:24:04 -0500

    Added my files

---
+++ main.py
@@ -0,0 +1,39 @@
+'''
+    Program: My Magical Calculator
+    Author: Cody York
+    Copyright: 2017
+'''
+
+import re
+
+print("***** My Magical Calculator ******")
+print("****** Type 'quit' to exit ****** \n")
+
+
+previous = 0
+run = True
+
+def performMath():
+    global run
+    global previous
+    equation = ""
+
+    #if there has been a previous calculation, use that result at the prompt
+    if previous == 0:
+        equation = input("Enter equation:")
+    else:
+        equation = input(str(previous))
+
+    #if user quits ->
+    if equation == 'quit':
+        print("Goodbye, human.")
+        run = False
+    else:
+        equation = re.sub('[a-zA-z,.:()" "]',' ', equation)
+        if previous == 0:
+            previous = eval(equation)
+        else:
+            previous = eval(str(previous) + equation)
+
+while run:
+    performMath()
